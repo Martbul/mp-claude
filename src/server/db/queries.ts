b01ae72baf4aa68bcbed8c98a6450a535b68c4f8
Martbul <martbul01@gmail.com>
2025-06-15 19:34:05 +0300

    cleanum with minimal data access layer

---
+++ src/server/db/queries.ts
@@ -0,0 +1,34 @@
+import "server-only";
+
+import { files_table, folders_table } from "~/server/db/schema";
+import { db } from "~/server/db";
+import { eq } from "drizzle-orm";
+
+export const QUERIES = {
+  //you dont have to async/await if you are returning a promise
+  getFolders: function(folderId: number) {
+    return db.select().from(folders_table).where(eq(folders_table.parent, folderId))
+  },
+
+
+  getFiles: function(folderId: number) {
+    return db.select().from(files_table).where(eq(files_table.parent, folderId))
+  },
+
+  getAllParentsForFolder: async function(folderId: number) {
+    const parents = []
+    let currentId: number | null = folderId
+    while (currentId !== null) {
+      const folder = await db.selectDistinct().from(folders_table).where(eq(folders_table.id, currentId))
+
+      if (!folder[0]) {
+        throw new Error("Parent folder not found!")
+      }
+
+      parents.unshift(folder[0])
+      currentId = folder[0]?.parent
+    }
+    return parents;
+  }
+
+}
